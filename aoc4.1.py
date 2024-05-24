054a5b28eaa29e427f262ad1d738edab5b8869cc
Jules <164848231+Jules-ndrw@users.noreply.github.com>
2024-05-24 16:48:30 +0800

    Add files via upload

---
+++ aoc4.1.py
@@ -0,0 +1,16 @@
+import hashlib
+
+og_key = "yzbqklnj"
+num = 1
+while True: #creating a loop
+    key = og_key + str(num)
+    #make into hash
+    keya = key.encode()
+    keyb = hashlib.md5(keya)
+    keyc = keyb.hexdigest()
+    #verify
+    if keyc.startswith("000000"):
+        print(num)
+        break
+    else:
+        num += 1
